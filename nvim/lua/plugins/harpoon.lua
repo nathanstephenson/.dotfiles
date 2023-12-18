5b94b9b4f133e9658d763c36773ceb233543bccc
nathanstephenson <nathans261@gmail.com>
2023-12-18 13:34:03 +0000

    update to harpoon2 with bindings
    needs setAt, but doesn't look like this is currently supported
    
    .

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,16 +1,25 @@
-return { 'ThePrimeagen/harpoon',
-    config = function()
-	Map("n", "<leader>ga", "<Cmd>lua require('harpoon.mark').add_file()<cr>")
-	Map("n", "<leader>g1", "<Cmd>lua require('harpoon.mark').set_current_at(1)<cr>")
-	Map("n", "<leader>g2", "<Cmd>lua require('harpoon.mark').set_current_at(2)<cr>")
-	Map("n", "<leader>g3", "<Cmd>lua require('harpoon.mark').set_current_at(3)<cr>")
-	Map("n", "<leader>g4", "<Cmd>lua require('harpoon.mark').set_current_at(4)<cr>")
-	Map("n", "<leader>rr", "<Cmd>lua require('harpoon.mark').rm_file()<cr>")
+return {
+    'ThePrimeagen/harpoon',
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    setup = true,
+    config = true,
+    init =  function()
+	local harpoon = require("harpoon")
+	harpoon:setup()
+	Map("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-	Map("n", "<leader>m", "<Cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
-	Map("n", "<leader>1", "<Cmd>lua require('harpoon.ui').nav_file(1)<cr>")
-	Map("n", "<leader>2", "<Cmd>lua require('harpoon.ui').nav_file(2)<cr>")
-	Map("n", "<leader>3", "<Cmd>lua require('harpoon.ui').nav_file(3)<cr>")
-	Map("n", "<leader>4", "<Cmd>lua require('harpoon.ui').nav_file(4)<cr>")
-    end
+	Map("n", "<leader>ga", function() harpoon:list():append() end)
+	Map("n", "<leader>rr", function() harpoon:list():remove() end)
+	Map("n", "<leader>ra", function() harpoon:list():clear() end)
+	-- Map("n", "<leader>g1", function() harpoon:list():set_current_at(1) end)
+	-- Map("n", "<leader>g2", function() harpoon:list():set_current_at(2) end)
+	-- Map("n", "<leader>g3", function() harpoon:list():set_current_at(3) end)
+	-- Map("n", "<leader>g4", function() harpoon:list():set_current_at(4) end)
+
+	Map("n", "<leader>1", function() harpoon:list():select(1) end)
+	Map("n", "<leader>2", function() harpoon:list():select(2) end)
+	Map("n", "<leader>3", function() harpoon:list():select(3) end)
+	Map("n", "<leader>4", function() harpoon:list():select(4) end)
+    end,
 }
