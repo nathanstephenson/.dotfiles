cf01b2121cb5ac1d646b9c484382487f404ccc88
nathanstephenson <nathans261@gmail.com>
2024-01-09 12:31:23 +0000

    fix neoformat autocmds

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -2,7 +2,7 @@ return {
     "sbdchd/neoformat",
     config = function()
 	vim.g.neoformat_try_node_exe = 1
-	Autocmd("NeofmtOnSave", { "BufWritePre", "InsertLeave" }, { "*.js", "*.ts", "*.tsx" }, "Neoformat prettier")
-	Autocmd("NeofmtOnSave", { "BufWritePre", "InsertLeave" }, { "*.go" }, "Neoformat gofmt")
+	Autocmd("NeofmtOnSave JS", { "BufWritePre", "InsertLeave" }, { "*.js", "*.ts", "*.tsx" }, "Neoformat prettier")
+	Autocmd("NeofmtOnSave Go", { "BufWritePre", "InsertLeave" }, { "*.go" }, "Neoformat gofmt")
     end
 }
