887dbfd3f81e762cb6a4fd97e40d2990deb669f2
nathanstephenson <nathans261@gmail.com>
2024-06-05 15:15:25 +0100

    fix harpoon for project-level marks

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,25 +1,50 @@
 return {
     'ThePrimeagen/harpoon',
     branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
     setup = true,
     config = true,
     init =  function()
-	local harpoon = require("harpoon")
-	harpoon:setup()
-	Map("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        local harpoon = require("harpoon")
+        harpoon:setup({
+            settings = {
+                key = vim.fn.FindRootDirectory
+            },
+            default = {
+                get_root_dir = vim.fn.FindRootDirectory,
+                select = function(list_item, list, option)
+                    local root = vim.fn.FindRootDirectory()
+                    print ("Opening " .. root .. list_item.value)
+                    vim.cmd("e " .. root .. "/" .. list_item.value)
+                end
+            }
+        })
+        --local conf = require("telescope.config").values
+        --local function toggle_telescope(harpoon_files)
+        --    local file_paths = {}
+        --    for _, item in ipairs(harpoon_files.items) do
+        --        table.insert(file_paths, item.value)
+        --    end
 
-	Map("n", "<leader>ga", function() harpoon:list():append() end)
-	Map("n", "<leader>rr", function() harpoon:list():remove() end)
-	Map("n", "<leader>ra", function() harpoon:list():clear() end)
-	-- Map("n", "<leader>g1", function() harpoon:list():set_current_at(1) end)
-	-- Map("n", "<leader>g2", function() harpoon:list():set_current_at(2) end)
-	-- Map("n", "<leader>g3", function() harpoon:list():set_current_at(3) end)
-	-- Map("n", "<leader>g4", function() harpoon:list():set_current_at(4) end)
+        --    require("telescope.pickers").new({}, {
+        --        prompt_title = "Harpoon",
+        --        finder = require("telescope.finders").new_table({
+        --            results = file_paths,
+        --        }),
+        --        previewer = conf.file_previewer({}),
+        --        sorter = conf.generic_sorter({}),
+        --    }):find()
+        --end
 
-	Map("n", "<leader>1", function() harpoon:list():select(1) end)
-	Map("n", "<leader>2", function() harpoon:list():select(2) end)
-	Map("n", "<leader>3", function() harpoon:list():select(3) end)
-	Map("n", "<leader>4", function() harpoon:list():select(4) end)
+        Map("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+        Map("n", "<leader>ga", function() harpoon:list():add() end)
+        Map("n", "<leader>gr", function() harpoon:list():remove() end)
+        Map("n", "<leader>ra", function() harpoon:list():clear() end)
+
+        Map("n", "<leader>1", function() harpoon:list():select(1) end)
+        Map("n", "<leader>2", function() harpoon:list():select(2) end)
+        Map("n", "<leader>3", function() harpoon:list():select(3) end)
+        Map("n", "<leader>4", function() harpoon:list():select(4) end)
     end,
 }
