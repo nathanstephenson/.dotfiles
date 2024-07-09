f1bd65dab6d5d5ce87847f3ee2992bc60704dab6
nathanstephenson <nathans261@gmail.com>
2024-07-09 23:24:15 +0100

    add rust

--- nvim/lua/plugins/neoformat.lua
+++ nvim/lua/plugins/neoformat.lua
@@ -4,6 +4,7 @@ return {
 	vim.g.neoformat_try_node_exe = 1
 	Autocmd("NeofmtOnSave JS", { "BufWritePre", "InsertLeave" }, { "*.js", "*.ts", "*.tsx" }, "Neoformat prettier")
 	Autocmd("NeofmtOnSave Go", { "BufWritePre", "InsertLeave" }, { "*.go" }, "Neoformat gofmt")
+	Autocmd("NeofmtOnSave Rust", { "BufWritePre", "InsertLeave" }, { "*.rs" }, "Neoformat rustfmt")
 	Autocmd("NeofmtOnSave Python", { "BufWritePre", "InsertLeave" }, { "*.py" }, "Neoformat")
     end
 }
