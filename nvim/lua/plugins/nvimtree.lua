06b3dc46d070d1d2db02eb1f959d5105dc682de4
nathanstephenson <nathans261@gmail.com>
2025-05-10 15:06:58 +0100

    update width of nvim-tree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -5,7 +5,11 @@ return {
 	},
 	config = function()
 		local nvimtree = require("nvim-tree")
-		nvimtree.setup({})
+		nvimtree.setup({
+			view = {
+				width = 60,
+			},
+		})
 
 		Map("n", "<leader>t", ":NvimTreeToggle<CR>", { desc = "Toggle NvimTree" })
 	end,
