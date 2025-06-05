097d14e36dd9714fb112567a1e20eb2c3b34c51b
nathanstephenson <nathans261@gmail.com>
2025-06-05 18:39:15 +0100

    ?

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,20 +1,20 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {
-        formatters_by_ft = {
-            go = { "gofmt" },
-            templ = { "gofmt" },
-            lua = { "stylua" },
-            javascript = { "biome", "prettier", stop_after_first = true },
-            javascriptreact = { "biome", "prettier", stop_after_first = true },
-            typescript = { "biome", "prettier", stop_after_first = true },
-            typescriptreact = { "biome", "prettier", stop_after_first = true },
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_format = "never",
-        },
-        notify_on_error = true,
-        notify_no_formatters = true,
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			go = { "gofmt" },
+			templ = { "gofmt" },
+			lua = { "stylua" },
+			javascript = { "biome", "prettier", stop_after_first = true },
+			javascriptreact = { "biome", "prettier", stop_after_first = true },
+			typescript = { "biome", "prettier", stop_after_first = true },
+			typescriptreact = { "biome", "prettier", stop_after_first = true },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "never",
+		},
+		notify_on_error = true,
+		notify_no_formatters = true,
+	},
 }
