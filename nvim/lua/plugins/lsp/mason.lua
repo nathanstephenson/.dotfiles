37d5d6af46c4c2116718bcc9810900b1d3d08901
nathanstephenson <nathans261@gmail.com>
2025-06-05 18:55:04 +0100

    don't auto-enable lsp with mason, formatting

--- nvim/lua/plugins/lsp/mason.lua
+++ nvim/lua/plugins/lsp/mason.lua
@@ -1,34 +1,35 @@
 return {
-    "williamboman/mason.nvim",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
-    config = function()
-	local mason = require("mason")
-	local mason_lspconfig = require("mason-lspconfig")
+	"williamboman/mason.nvim",
+	dependencies = { "williamboman/mason-lspconfig.nvim" },
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
-	mason.setup({
-	    ensure_installed = {
-            --formatter
-	    }
-	})
+		mason.setup({
+			ensure_installed = {
+				--formatter
+			},
+		})
 
-	mason_lspconfig.setup({
-	    ensure_installed = {
-            "vtsls",
-            "html",
-            "cssls",
-            "emmet_ls",
-            "rust_analyzer",
-            "kotlin_language_server",
-            "vimls",
-            "lua_ls",
-            "gopls",
-            "templ",
-            "tailwindcss",
-            "pyright",
-            "rust_analyzer",
-            "eslint",
-	    },
-	    automatic_installation = true
-	})
-    end
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"vtsls",
+				"html",
+				"cssls",
+				"emmet_ls",
+				"rust_analyzer",
+				"kotlin_language_server",
+				"vimls",
+				"lua_ls",
+				"gopls",
+				"templ",
+				"tailwindcss",
+				"pyright",
+				"rust_analyzer",
+				"eslint",
+			},
+			automatic_installation = true,
+			automatic_enable = false,
+		})
+	end,
 }
