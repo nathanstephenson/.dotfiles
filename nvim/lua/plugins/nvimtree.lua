dd70c1310269a595fd7844f1d150599b3de156aa
nathanstephenson <nathans261@gmail.com>
2025-06-30 19:39:40 +0100

    update nvim-tree binds

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -12,8 +12,11 @@ return {
 		})
 
 		local api = require("nvim-tree.api")
-		Map("n", "<leader>t", function()
+		Map("n", "<leader>tt", function()
 			api.tree.toggle({ path = vim.fn.FindRootDirectory(), update_root = false, find_file = true })
+		end, { desc = "Toggle NvimTree" })
+		Map("n", "<leader>tj", function()
+			api.tree.find_file({ update_root = false, open = true, focus = true })
 		end, { desc = "Toggle NvimTree" })
 	end,
 }
